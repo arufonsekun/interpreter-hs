f8ca559419a4e872ca42aec8073ce66856facebb
Junior V. Ramisch <junior.ramisch@gmail.com>
2022-04-01 12:49:14 -0300

    refact: Improves evaluate function

--- Interpreter/AST.hs
+++ Interpreter/AST.hs
@@ -1,16 +1,19 @@
 module Interpreter.AST where
 
-data EXPR = TRUE
+data Expr = TRUE
             | FALSE
             | NUM Int
-            | ADD EXPR EXPR
-            | AND EXPR EXPR
-            | IF  EXPR EXPR EXPR
-        deriving Show
+            | ADD Expr Expr
+            | AND Expr Expr
+            deriving (Show, Eq)
+            -- | IF  Expr Expr Expr
 
 data TYPE = T_NUM
           | T_BOOL
-          | T_ADD
-          | T_AND
-          | T_IF
           deriving Show
+
+-- Construtor viável de adicionar:
+-- 1. t1 ; t2 -> expressões em sequência
+-- 2. let -> atribuir um nome a uma expressão
+-- 3. pairs -> tipo de dados composto
+-- 4. record -> {x=5, y=2}, me parece complicado
--- Interpreter/Analyzer.hs
+++ Interpreter/Analyzer.hs
@@ -1,7 +1,8 @@
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 module Interpreter.Analyzer where
 import Interpreter.AST
 
-step :: EXPR -> EXPR
+step :: Expr -> Expr
 step (ADD (NUM n1) (NUM n2)) = NUM (n1 + n2)
 step (ADD (NUM n1) e2) = case step e2 of
                           e2' -> ADD (NUM n1) e2'
@@ -12,47 +13,35 @@ step (AND FALSE _) = FALSE
 step (AND _ FALSE) = FALSE
 step (AND e1 e2) = case step e1 of
                     e1' -> AND e1' e2
-step (IF TRUE e1 _) = e1
-step (IF FALSE _ e2) = e2
-step (IF e e1 e2) = case step e of
-                     e' -> IF e' e1 e2
+-- step (IF TRUE e1 _) = e1
+-- step (IF FALSE _ e2) = e2
+-- step (IF e e1 e2) = case step e of
+--                      e' -> IF e' e1 e2
 step e = e
 
-typeof :: EXPR -> Maybe TYPE
+typeof :: Expr -> Maybe TYPE
 typeof TRUE = Just T_BOOL
 typeof FALSE = Just T_BOOL
 typeof (NUM _) = Just T_NUM
-typeof (ADD (NUM n1) e1) = Just T_ADD
-typeof (ADD e1 (NUM n1)) = Just T_ADD
 typeof (ADD e1 e2) = case (typeof e1, typeof e2) of
                         (Just T_NUM, Just T_NUM) -> Just T_NUM
-                        (Just T_NUM, Just T_ADD) -> Just T_ADD
-                        (Just T_ADD, Just T_NUM) -> Just T_ADD
-                        (Just T_ADD, Just T_ADD) -> Just T_ADD
                         _              -> Nothing
-typeof (AND TRUE e1) = Just T_AND
-typeof (AND e1 TRUE) = Just T_AND
-typeof (AND FALSE e1) = Just T_AND
-typeof (AND e1 FALSE) = Just T_AND
 typeof (AND e1 e2) = case (typeof e1, typeof e2) of
                         (Just T_BOOL, Just T_BOOL) -> Just T_BOOL
-                        (Just T_AND, Just T_BOOL)  -> Just T_AND
-                        (Just T_BOOL, Just T_AND)  -> Just T_AND
-                        (Just T_AND, Just T_AND)  -> Just T_AND
                         _                -> Nothing
-typeof (IF _ e1 e2) = case (typeof e1, typeof e2) of
-                        (Just T_NUM, Just T_NUM)   -> Just T_NUM
-                        (Just T_ADD, Just T_NUM)   -> Just T_ADD
-                        (Just T_NUM, Just T_ADD)   -> Just T_ADD
-                        (Just T_ADD, Just T_ADD)   -> Just T_ADD
-                        (Just T_BOOL, Just T_BOOL) -> Just T_BOOL
-                        (Just T_AND, Just T_BOOL)  -> Just T_AND
-                        (Just T_BOOL, Just T_AND)  -> Just T_AND
-                        (Just T_AND, Just T_AND)  -> Just T_AND
-                        _                          -> Nothing
+-- typeof (IF _ e1 e2) = case (typeof e1, typeof e2) of
+--                         (Just T_NUM, Just T_NUM)   -> Just T_NUM
+--                         (Just T_ADD, Just T_NUM)   -> Just T_ADD
+--                         (Just T_NUM, Just T_ADD)   -> Just T_ADD
+--                         (Just T_ADD, Just T_ADD)   -> Just T_ADD
+--                         (Just T_BOOL, Just T_BOOL) -> Just T_BOOL
+--                         (Just T_AND, Just T_BOOL)  -> Just T_AND
+--                         (Just T_BOOL, Just T_AND)  -> Just T_AND
+--                         (Just T_AND, Just T_AND)  -> Just T_AND
+--                         _                          -> Nothing
 
 -- Professor's typeof
--- typeof :: EXPR -> Maybe TYPE
+-- typeof :: Expr -> Maybe TYPE
 -- typeof TRUE = Just T_BOOL
 -- typeof FALSE = Just T_BOOL
 -- typeof (NUM _) = Just T_NUM
@@ -69,14 +58,15 @@ typeof (IF _ e1 e2) = case (typeof e1, typeof e2) of
 --                             (Just T_BOOL, Just T_BOOL) -> Just T_BOOL
 --                             _     -> Nothing
 
-evaluate :: EXPR -> Maybe EXPR
-evaluate TRUE = Just TRUE
-evaluate FALSE = Just FALSE
-evaluate (NUM e) = Just (NUM e)
-evaluate exp = case typeof exp of
-                 Just T_ADD  -> evaluate(step exp)
-                 Just T_AND  -> evaluate(step exp)
-                 Just T_IF   -> evaluate(step exp)
-                 Just T_BOOL -> Just exp
-                 Just T_NUM  -> Just exp
-                 _           -> Nothing
+evaluate :: Expr -> Expr
+evaluate e = case step e of
+                e' -> if e' == e
+                        then e
+                        else evaluate e'
+                _ -> error "Semantic error: erro avaliando a expressão"
+ 
+
+typecheck :: Expr -> Expr
+typecheck e = case typeof e of 
+                Just _ -> e
+                _ -> error "Type error: erro na verificação de tipos."
