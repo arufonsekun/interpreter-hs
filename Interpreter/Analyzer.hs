b1bd117fab02a816dc0e55b54e0d6d04694e1435
Junior V. Ramisch <junior.ramisch@gmail.com>
2022-04-03 00:17:56 -0300

    feat: Adjusts step function in order to handle pair constructor
    TODO: tests, adjust typeof function

--- Interpreter/Analyzer.hs
+++ Interpreter/Analyzer.hs
@@ -17,6 +17,39 @@ step (IF TRUE e1 _) = e1
 step (IF FALSE _ e2) = e2
 step (IF e e1 e2) = case step e of
                      e' -> IF e' e1 e2
+-- (E-PairBeta1)
+step (FIRST (PAIR (NUM n1) _)) = NUM n1
+step (FIRST (PAIR TRUE _)) = TRUE
+step (FIRST (PAIR FALSE _)) = FALSE
+
+step (FIRST (PAIR e1 _)) = case step e1 of
+                            e1' ->  e1'
+-- (E-Proj1)
+step (FIRST e) = case step e of
+                  e' -> FIRST e'
+
+-- (E-PairBeta2)
+step (SECOND (PAIR _ (NUM n1))) = NUM n1
+step (SECOND (PAIR _ TRUE)) = TRUE
+step (SECOND (PAIR _ FALSE)) = FALSE
+
+step (SECOND (PAIR _ e1)) = case step e1 of
+                         e1' ->  e1'
+-- (E-Proj2)
+step (SECOND e) = case step e of
+                  e' -> SECOND e'
+
+-- (E-Pair2)
+step (PAIR (NUM n1) e2) = case step e2 of
+                           e2' -> PAIR (NUM n1) e2'
+step (PAIR FALSE e2) = case step e2 of
+                           e2' -> PAIR FALSE e2'
+step (PAIR TRUE e2) = case step e2 of
+                           e2' -> PAIR TRUE e2'
+-- (E-Pair1)
+step (PAIR e1 e2) = case step e1 of
+                      e1' -> PAIR e1' e2
+
 step e = e
 
 typeof :: Expr -> Maybe TYPE
@@ -44,6 +77,6 @@ evaluate e = case step e of
                 _ -> error "Semantic error: erro avaliando a expressão"
 
 typecheck :: Expr -> Expr
-typecheck e = case typeof e of 
+typecheck e = case typeof e of
                 Just _ -> e
                 _ -> error "Type error: erro na verificação de tipos."
