1397a6986c75f4da81abe1989616ea04ac3bf381
Junior V. Ramisch <junior.ramisch@gmail.com>
2022-04-12 20:46:23 -0300

    Removes dead code

--- Main.hs
+++ Main.hs
@@ -3,7 +3,3 @@ import Interpreter.Analyzer
 import Interpreter.Parser
 
 main = getContents >>= print . evaluate . typecheck . parser . lexer 
--- step (ADD (NUM 1) (NUM 2))
--- step (ADD (NUM 1) (ADD (NUM 2) (NUM 3)))
--- step (IF FALSE (NUM 1) (NUM 2))
--- step (IF TRUE (NUM 1) (NUM 2))
